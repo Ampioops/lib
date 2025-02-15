b77e33bb8107254a092444d4daf1a5e78454ba5e
Ampioops <99460994+Ampioops@users.noreply.github.com>
2025-02-15 10:41:20 +0500

    подготовил боевую к запуску в контейнере пока не проверял

--- src/test/java/com/lib_for_mentor/lib_for_mentor/controller/AuthorControllerTest.java
+++ src/test/java/com/lib_for_mentor/lib_for_mentor/controller/AuthorControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.junit.jupiter.Testcontainers;
